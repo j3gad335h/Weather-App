b8a560018108b808659ca95d3ad71396f63e2a18
Jegadeesh <101029948+j3gad335h@users.noreply.github.com>
2023-05-27 09:59:18 +0530

    third commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    basePath: '/gh-pages-test',
+    experimental: {
+        appDir: true,
+    },
+}
 
 // next.config.js
 module.exports = {
+    ...nextConfig,
     images: {
         domains: ['openweathermap.org'],
     },
